e534a9dcae87a46e8f2b6cf58fa293b21288506e
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2024-07-26 12:52:35 -0700

    Upload

---
+++ radmc3d/const_opac-heat-log/mint2npz.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#===============================================================================
+# mint2npz.py
+#
+# Uses the radmc3dPy module to read `./mean_intensity.out`, integrate it over
+# all frequencies, and save it to tot_mint.npz.
+#
+# Author: Stanley A. Baronett
+# Created: 2024-07-25
+# Updated: 2024-07-26
+#===============================================================================
+import numpy as np
+from radmc3dPy import *
+from scipy import integrate
+
+data = analyze.readData(mint=True)
+print('Integrating over all frequencies', flush=True)
+tot_mint = integrate.trapezoid(data.meanint, x=data.freq, axis=-1)
+print('Saving to tot_mint.npz', flush=True)
+np.savez_compressed('tot_mint', tot_mint=tot_mint)
