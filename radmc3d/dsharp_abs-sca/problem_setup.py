b4d65c6adf778c518940090e7a99fc4f91bcf127
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2024-10-22 08:41:33 -0700

    Update and rerun on rusty

--- radmc3d/dsharp_abs-sca/problem_setup.py
+++ radmc3d/dsharp_abs-sca/problem_setup.py
@@ -8,29 +8,30 @@
 #
 # Author: Stanley A. Baronett
 # Created: 2023-12-11
-# Updated: 2024-04-04
+# Updated: 2024-10-22
 #===============================================================================
 import numpy as np
+import sys
+sys.path.insert(0, '/mnt/home/sbaronett/github/PrincetonUniversity/athena/vis/python')
+# sys.path.insert(0, '/home/stanley/github/PrincetonUniversity/athena/vis/python')
+import athena_read
 
 # BEGIN `amr_grid.inp`==========================================================
-# Constants
-au       = 1.495978707e13                     # Astronomical Unit       [cm]
-
-# Grid parameters
-nr       = 256
-ntheta   = 512
-nphi     = 1
-rin      = 10*au                              # [cm]
-rout     = 100*au                             # [cm]
-thetamin = 0
-thetamax = np.pi
-phimin   = 0
-phimax   = 2*np.pi
-
-# Make the coordinates
-ri       = np.linspace(rin, rout, nr+1)
-thetai   = np.linspace(thetamin, thetamax, ntheta+1)
-phii     = np.linspace(phimin, phimax, nphi+1)
+# Read the Athena++ grid
+run = 'nfreq/3/v2'
+problem_id = 'dsharp'
+# path = '/home/stanley/github/sabaronett/rad-hydro/athena/dev/yanfeij/'\
+path = '/mnt/home/sbaronett/ceph/github/sabaronett/rad-hydro/athena/dev/yanfeij/'\
+       +f'{problem_id}_abs-sca/{run}'
+athinput = athena_read.athinput(f'{path}/athinput.{problem_id}')
+length_unit = athinput['radiation']['length_unit']
+athdf = athena_read.athdf(f'{path}/athdf/{problem_id}.out1.00000.athdf')
+nr = len(athdf['x1v'])
+ntheta = len(athdf['x2v'])
+nphi = len(athdf['x3v'])
+ri = athdf['x1f']*length_unit
+thetai = athdf['x2f']
+phii = athdf['x3f']
 
 # Write the grid file
 # https://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/manual_radmc3d/inputoutputfiles.html#input-required-amr-grid-inp
@@ -42,108 +43,67 @@ with open('amr_grid.inp', 'w+') as f:
     f.write('1 1 0\n')                        # Include r,theta coordinates
     f.write(f'{nr:d} {ntheta:d} {nphi:d}\n')  # Size of grid
     for value in ri:
-        f.write(f'{value:13.6e}\n')           # X coordinates (cell walls)
+        f.write(f'{value:.16e}\n')            # X coordinates (cell walls)
     for value in thetai:
-        f.write(f'{value:13.6e}\n')           # Y coordinates (cell walls)
+        f.write(f'{value:.16e}\n')            # Y coordinates (cell walls)
     for value in phii:
-        f.write(f'{value:13.6e}\n')           # Z coordinates (cell walls)
+        f.write(f'{value:.16e}\n')            # Z coordinates (cell walls)
 # END `amr_grid.inp`============================================================
 
 
 # BEGIN `wavelength_micron.inp`=================================================
 # Wavelength grid
-lam1     = 0.1e0
-lam2     = 7.0e0
-lam3     = 25.e0
-lam4     = 1.0e4
-n12      = 20
-n23      = 100
-n34      = 30
-lam12    = np.logspace(np.log10(lam1), np.log10(lam2), n12, endpoint=False)
-lam23    = np.logspace(np.log10(lam2), np.log10(lam3), n23, endpoint=False)
-lam34    = np.logspace(np.log10(lam3), np.log10(lam4), n34, endpoint=True)
-lam      = np.concatenate([lam12, lam23, lam34])
-nlam     = lam.size
+lam1 = 0.1e0
+lam2 = 7.0e0
+lam3 = 25.e0
+lam4 = 1.0e4
+n12 = 20
+n23 = 100
+n34 = 30
+lam12 = np.logspace(np.log10(lam1), np.log10(lam2), n12, endpoint=False)
+lam23 = np.logspace(np.log10(lam2), np.log10(lam3), n23, endpoint=False)
+lam34 = np.logspace(np.log10(lam3), np.log10(lam4), n34, endpoint=True)
+lam = np.concatenate([lam12, lam23, lam34])
+nlam = lam.size
 
 # Write the wavelength file
 # https://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/manual_radmc3d/inputoutputfiles.html#input-required-wavelength-micron-inp
 with open('wavelength_micron.inp', 'w+') as f:
     f.write(f'{nlam:d}\n')
     for value in lam:
-        f.write(f'{value:13.6e}\n')
+        f.write(f'{value:.16e}\n')
 # END `wavelength_micron.inp`===================================================
 
 
 # BEGIN `stars.inp`=============================================================
 # Constants
-ms       = 1.98892e33                         # Solar mass              [g]
-ts       = 5.78388e3                          # Solar temperature       [K]
-rs       = 6.9368e10                          # Solar radius            [cm]
+ms = 1.98892e33                               # Solar mass [g]
+ts = 5.78388e3                                # Solar temperature [K]
+rs = 6.9368e10                                # Solar radius [cm]
 
 # Star parameters
-mstar    = ms                                 # unimportant as of verion 2.0
-rstar    = rs                                 # for extended source treatment
-tstar    = ts                                 # blackbody temperature
-pstar    = np.array([0.,0.,0.])               # position
+mstar = ms                                    # unimportant as of verion 2.0
+rstar = rs                                    # for extended source treatment
+tstar = ts                                    # blackbody temperature
+pstar = np.array([0.,0.,0.])                  # position
 
 # Write the stars.inp file
 # https://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/manual_radmc3d/inputoutputfiles.html#sec-stars
 with open('stars.inp', 'w+') as f:
     f.write('2\n')
     f.write(f'1 {nlam:d}\n\n')
-    f.write(f'{rstar:13.6e} {mstar:13.6e} {pstar[0]:13.6e} {pstar[1]:13.6e} '\
-            + f'{pstar[2]:13.6e}\n\n')
+    f.write(f'{rstar:.16e} {mstar:.16e} {pstar[0]:.16e} {pstar[1]:.16e} '\
+            + f'{pstar[2]:.16e}\n\n')
     for value in lam:
-        f.write(f'{value:13.6e}\n')
-    f.write(f'\n{-tstar:13.6e}\n')
+        f.write(f'{value:.16e}\n')
+    f.write(f'\n{-tstar:.16e}\n')
 # END `stars.inp`===============================================================
 
 
 # BEGIN `dust_density.inp`======================================================
 # Make the cell-centered grid
-rc       = 0.5*(ri[0:nr] + ri[1:nr+1])
-thetac   = 0.5*(thetai[0:ntheta] + thetai[1:ntheta+1])
-phic     = 0.5*(phii[0:nphi] + phii[1:nphi+1])
-qq       = np.meshgrid(rc, thetac, indexing='ij')
-rr       = qq[0]
-tt       = qq[1]
-# zr       = np.pi/2.e0 - qq[1]
-
-# Disk parameters
-gm0      = 1.0                                # GM
-T_unit   = 6.14e3                             # T_0 [K]
-density_unit = 4.28e-14                       # \rho_0 [g/cm^3]
-length_unit = 5.98e14                         # L_0 [cm]
-dfloor   = 1e-12                              # minimum density [g/cm^3]
-r0       = 0.425278227742474                  # disk radial normalization [L_0]
-rho0     = 0.2                                # disk density normalization [\rho_0]
-p0_over_r0 = 4.80e-03                         # (H/r0)^2
-pslope   = -0.5                               # pressure power-law index
-dslope   = -2.25                              # density power-law index
-
-# Make the dust density model
-def GetCylCoord(x1, x2, x3):
-    rad = np.abs(x1*np.sin(x2))
-    phi = x3
-    z = x1*np.cos(x2)
-    return rad, phi, z
-
-def DenProfileCyl(rad, phi, z):
-    p_over_r = PoverR(rad, phi, z)
-    denmid = rho0*np.power((rad + r0)/r0, dslope)\
-            /(1 + np.exp(-np.exp(np.e)*(rad - r0)/r0))
-    dentem = denmid*np.exp(gm0/p_over_r*(1./np.sqrt(rad**2 + z**2) - 1./rad))
-    den = dentem
-    return den
-
-def PoverR(rad, phi, z):
-    poverr = p0_over_r0*np.power(rad/r0, pslope)
-    return poverr
-
-rads, phis, zs = GetCylCoord(rr/length_unit, tt, 0)
-rhod     = DenProfileCyl(rads, phis, zs)
-rhod[rhod < dfloor] = dfloor
-rhod     = rhod*density_unit
+density_unit = athinput['radiation']['density_unit']
+rhod = athdf['rho']*density_unit
 
 # Write the density file
 # https://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/manual_radmc3d/inputoutputfiles.html#input-required-for-dust-transfer-dust-density-inp
@@ -151,8 +111,8 @@ with open('dust_density.inp', 'w+') as f:
     f.write('1\n')                            # Format number
     f.write(f'{nr*ntheta*nphi:d}\n')          # Nr of cells
     f.write('1\n')                            # Nr of dust species
-    data = rhod.ravel(order='F')              # Fortran-style indexing
-    data.tofile(f, sep='\n', format='%13.6e')
+    data = rhod.ravel()
+    data.tofile(f, sep='\n', format='%.16e')
     f.write('\n')
 # END `dust_density.inp`========================================================
 
@@ -166,16 +126,16 @@ with open('dustopac.inp', 'w+') as f:
     f.write('============================================================================\n')
     f.write('1               inputstyle (Way in which this dust species is read)\n')
     f.write('0               iquantum (0=Thermal grain)\n')
-    # f.write('silicate        Extension of name of dustkappa_***.inp file\n')
-    f.write('dsharp_abs-sca  Extension of name of dustkappa_***.inp file\n')
+    f.write('dsharp      Extension of name of dustkappa_***.inp file\n')
     f.write('----------------------------------------------------------------------------\n')
 # END `dustopac.inp`============================================================
 
 
 # BEGIN `radmc3d.inp`===========================================================
 # Monte Carlo parameters
-nphot    = int(1e8)
-countwrite = int(1e5)
+nphot    = int(8e7)
+nphot_mono = int(1e6)
+countwrite = int(1e6)
 
 # Write the radmc3d.inp control file
 # https://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/manual_radmc3d/clioptions.html#additional-arguments-general
@@ -183,8 +143,32 @@ with open('radmc3d.inp', 'w+') as f:
     f.write(f'countwrite = {countwrite}\n')   # nr photons between std. outputs
     f.write('iranfreqmode = 1\n')             # differ RNG seeds per OMP process
     f.write('istar_sphere = 0\n')             # point (0)/sphere (1) star source
-    f.write('mc_weighted_photons = 0\n')      # focus photons toward grid
     f.write(f'nphot = {nphot:d}\n')           # number of photons
-    f.write('scattering_mode_max = 1\n')      # isotropic scattering
-    f.write('setthreads = 18\n')              # Intel Core i7-12700H optimized
+    f.write(f'nphot_mono = {nphot_mono:d}\n') # number of monochromatic photons
+    f.write('scattering_mode_max = 1\n')      # no scattering (zero dust albedo)
+    f.write('setthreads = 122\n')             # AMD Rome optimized
 # END `radmc3d.inp`=============================================================
+
+
+# BEGIN `mcmono_wavelength_micron.inp`=================================================
+# Wavelength grid
+lam1 = 0.1e0
+lam2 = 7.0e0
+lam3 = 25.e0
+lam4 = 1.0e4
+n12 = 20
+n23 = 100
+n34 = 30
+lam12 = np.logspace(np.log10(lam1), np.log10(lam2), n12, endpoint=False)
+lam23 = np.logspace(np.log10(lam2), np.log10(lam3), n23, endpoint=False)
+lam34 = np.logspace(np.log10(lam3), np.log10(lam4), n34, endpoint=True)
+lam = np.concatenate([lam12, lam23, lam34])
+nlam = lam.size
+
+# Write the wavelength file
+# https://www.ita.uni-heidelberg.de/~dullemond/software/radmc-3d/manual_radmc3d/dustradtrans.html#special-purpose-feature-computing-the-local-radiation-field
+with open('mcmono_wavelength_micron.inp', 'w+') as f:
+    f.write(f'{nlam:d}\n')
+    for value in lam:
+        f.write(f'{value:.16e}\n')
+# END `mcmono_wavelength_micron.inp`===================================================
